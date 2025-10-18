67fc39f44dbb59b087a6e44e68e78e519a8c8b16
Jakub <frontend@mediatool.tv>
2025-10-18 15:02:24 +0200

    update mobile menu

--- src/components/Shared/MobileMenu.tsx
+++ src/components/Shared/MobileMenu.tsx
@@ -1,5 +1,4 @@
-// components/MobileMenu.tsx
-import { Menu, X } from "lucide-react";
+import { Menu, X, MapPin, ClockAlert } from "lucide-react";
 import {
   Sheet,
   SheetContent,
@@ -8,6 +7,13 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 import { Separator } from "@radix-ui/react-menubar";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import Logo from "@assets/icons/Car1Logo.svg?react";
 
 type NavLink = {
   link?: string;
@@ -38,7 +44,7 @@ export default function MobileMenu({ links, acf }: Props) {
         >
           <div className="flex justify-between items-center mb-6">
             <SheetTitle className="text-xl font-bold text-white">
-            Меню
+              Меню
             </SheetTitle>
             <SheetClose className="text-white">
               <X size={24} />
@@ -46,38 +52,56 @@ export default function MobileMenu({ links, acf }: Props) {
           </div>
 
           <div className="flex flex-col mb-4 text-white">
-            <a
-              href={`tel:${acf.phone}`}
-              className="text-sm text-white/80 font-semibold"
-            >
-              {acf.phone}
-            </a>
-            <p className="text-xs">{acf.address}</p>
-            <p className="text-xs">{acf.work_time}</p>
+            <div>
+              <a
+                href={`tel:${acf.phone}`}
+                className="text-sm text-white/80 font-semibold"
+              >
+                {acf.phone}
+              </a>
+              <p className="text-xs text-white flex gap-2 items-center">
+                <MapPin size="14" />
+                {acf.address}
+              </p>
+              <p className="text-xs text-white flex gap-2 items-center">
+                <ClockAlert size="14" />
+                {acf.work_time}
+              </p>
+            </div>
           </div>
           <Separator className="bg-white/20 w-full mb-4 h-px" />
           <nav className="flex flex-col gap-2">
             {links.map((link, idx) =>
               link.childs ? (
-                <div key={`sub-${idx}`}>
-                  <p className="font-semibold text-base mb-2 text-white">
-                    {link.name}
-                  </p>
-                  <ul className="pl-3 space-y-2">
-                    {link.childs.map((child, subIdx) => (
-                      <li key={`child-${idx}-${subIdx}`}>
-                        <SheetClose asChild>
-                          <a
-                            href={child.link}
-                            className="block text-base text-white/70"
-                          >
-                            {child.name}
-                          </a>
-                        </SheetClose>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
+                <Accordion
+                  type="single"
+                  collapsible
+                  className="w-full"
+                  key={`sub-${idx}`}
+                >
+                  <AccordionItem value="item-1" className="w-fit">
+                    <AccordionTrigger className="font-semibold text-base text-white py-0 mb-0 justify-start">
+                      {" "}
+                      {link.name}
+                    </AccordionTrigger>
+                    <AccordionContent className="bg-transparent px-0 py-2">
+                      <ul className="pl-3 space-y-2">
+                        {link.childs.map((child, subIdx) => (
+                          <li key={`child-${idx}-${subIdx}`}>
+                            <SheetClose asChild>
+                              <a
+                                href={child.link}
+                                className="block text-base text-white/70"
+                              >
+                                {child.name}
+                              </a>
+                            </SheetClose>
+                          </li>
+                        ))}
+                      </ul>
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
               ) : (
                 <SheetClose key={`item-${idx}`} asChild>
                   <a
