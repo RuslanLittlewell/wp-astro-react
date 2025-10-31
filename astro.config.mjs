8815a7e2dc6f2b55c102c52117ab411b8175b1c8
Jakub <frontend@mediatool.tv>
2025-10-31 01:10:08 +0100

    Fix form and add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { fileURLToPath } from "node:url";
 import { imageService } from "@unpic/astro/service";
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  site: "https://car1.by",
+  integrations: [react(), tailwind(), sitemap()],
   server: {
     port: 4321,
     host: true,
